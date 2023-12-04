ef218eda041112d5b2eae92bab86f8dcd7bfacce
dourcanary <semadavodanya@gmail.com>
2023-12-04 19:37:01 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,103 @@
+
+const fs = require('fs');
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const fastcsv = require('fast-csv');
+const UserAgent = require('user-agents');
+puppeteer.use(StealthPlugin());
+const { parse_NSBank, parse_SovComBank } = require('./parsers');
+
+async function scrapeBankWebsite(bank_name, url, isImposter) {
+  try {
+    const browser = await puppeteer.launch({ headless: 'new'}); // false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+
+    // Create a new random user-agent
+    const userAgent = new UserAgent();
+    const userAgentData = userAgent.data;
+
+    // Set up headers with userAgent data
+    const headers = {
+      "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+      "Accept-Encoding": 'gzip, deflate, br',
+      "Accept-Language": 'en-US,en;q=0.9',
+      "Cache-Control": 'max-age=0',
+      "Sec-Ch-Ua": `"${userAgentData.vendor};v="${userAgentData.browserVersion}, "Not?A_Brand";v="24"`,
+      "Sec-Ch-Ua-Mobile": '?0',
+      "Sec-Ch-Ua-Platform": `"${userAgentData.platform}"`,
+      "Sec-Fetch-Dest": 'document',
+      "Sec-Fetch-Mode": 'navigate',
+      "Sec-Fetch-Site": 'same-origin',
+      "Sec-Fetch-User": '?1',
+      "Upgrade-Insecure-Requests": '1',
+      "User-Agent": userAgent.toString(),
+    };
+    await page.setExtraHTTPHeaders(headers);
+
+    // Set viewport to match the userAgent's viewport dimensions
+    await page.setViewport({ 
+      width: userAgentData.viewportWidth, 
+      height: userAgentData.viewportHeight 
+    });
+
+    // Disable WebDriver flag
+    await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, 'webdriver', {
+        get: () => false,
+      });
+    });
+
+    let data;
+
+    if (bank_name === 'NSBank') {
+        data = await parse_NSBank(page, url, isImposter);
+    } else if (bank_name === 'SovComBank') {
+        data = await parse_SovComBank(page, url, isImposter);
+    }
+
+    await browser.close();
+    return data;
+  } catch (error) {
+      console.error(`Error scraping ${url}:`, error);
+      return null;
+  }
+}
+
+function writeDataToCSV(data, includeHeaders) {
+  const csvFilePath = "data.csv";
+  const csvStream = fastcsv.format({ headers: includeHeaders, includeEndRowDelimiter: true });
+
+  if (fs.existsSync(csvFilePath) && !includeHeaders) {
+    csvStream.pipe(fs.createWriteStream(csvFilePath, { flags: 'a' }));
+  } else {
+    csvStream.pipe(fs.createWriteStream(csvFilePath));
+  }
+
+  data.forEach(row => csvStream.write({ 
+    bank_name: row.bank_name, 
+    gold_weight: row.grams, 
+    price: row.price 
+  }));
+  csvStream.end();
+}
+
+(async () => {
+  let banksToScrape = [];
+  const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    console.log("Reading from bank_list.json...");
+    banksToScrape = JSON.parse(fs.readFileSync('bank_list.json', 'utf8'));
+  } else {
+    const [bankName, bankURL, isImposter] = args;
+    banksToScrape.push({ bankName, bankURL, isImposter});
+  }
+
+  let includeHeaders = true;
+  for (const bank of banksToScrape) {
+    const data = await scrapeBankWebsite(bank.bankName, bank.bankURL, bank.isImposter);
+    writeDataToCSV(data, includeHeaders);
+    includeHeaders = false;
+  }
+})();
---
+++ parsers.js
@@ -0,0 +1,67 @@
+const cheerio = require('cheerio');
+
+async function parse_NSBank(page, url, isImposter) {
+
+    console.log('Navigating to the data URL...');
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    // Mimic human-like interactions if imposter
+    if (isImposter) {
+        await randomHumanDelay(page);
+    }
+
+    // Existing parsing logic for NSBank
+    const data = [];
+    const items = await page.$$('body > main > div:nth-child(2) > div:nth-child(2) > div > div > div.row > div.col-md-8.my-3 > div' + ' .currency__item');
+    for (let item of items) {
+        const grams = await page.evaluate(el => el.querySelector('.currency__operation-name').textContent.trim(), item);
+        const price = await page.evaluate(el => el.querySelector('.currency__operation-price').textContent.trim(), item);
+        data.push({ bank_name: 'NSBank', grams, price: price.replace(/\s/g, '') });
+    }
+    return data;
+}
+
+async function parse_SovComBank(page, url, isImposter) {
+    console.log('Navigating to the data URL...');
+    await page.goto(url);
+
+    // Mimic human-like interactions if imposter
+    if (isImposter) {
+        await randomHumanDelay(page);
+    }
+
+    // Wait for the specific selector
+    await page.waitForSelector('#table-metals > div:nth-child(3) > div > div > div:nth-child(2)');
+    const elementHandle = await page.$('#table-metals > div:nth-child(3) > div > div > div:nth-child(2)');
+    const htmlContent = await elementHandle.evaluate(node => node.innerHTML);
+
+    console.log(htmlContent);
+    const $ = cheerio.load(htmlContent);
+    const data = [];
+
+    // Iterate through each row of the table
+    $('div.grid.items-center.grid-cols-5.py-4').each((index, element) => {
+        const cells = $(element).find('.leading-snug');
+        const grams = cells.eq(0).text().trim(); // Номинал слитка, грамм
+        const price = cells.eq(2).text().trim(); // Цена продажи, ₽ за слиток
+        data.push({ 
+            bank_name: 'SovComBank', 
+            grams, 
+            price: price.replace(/\s/g, '') 
+        });
+    
+});
+
+async function randomHumanDelay(page) {
+    await page.waitForTimeout(Math.random() * 1000 + 3000);
+    await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+  }
+
+console.log(data);
+return data;
+}
+
+module.exports = {
+  parse_NSBank,
+  parse_SovComBank
+};
